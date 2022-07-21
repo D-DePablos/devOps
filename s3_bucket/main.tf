e90ae2acb645b7d6d5f6f74c432607dae2458fb3
Diego de Pablos <d.depablosaguero@gmail.com>
2022-07-21 13:39:26 +0100

    Added valid S3 bucket creation

--- s3_bucket/main.tf
+++ s3_bucket/main.tf
@@ -1,3 +1,28 @@
 provider "aws" {
-  region = "eu-west-2"
+	region = var.s3_region
+}
+
+resource "random_string" "random" {
+  length  = 12
+  upper   = false
+  numeric  = false
+  lower   = true
+  special = false
+}
+
+resource "aws_s3_bucket" "b" {
+	bucket = "${var.s3_bucket_name}-${random_string.random.result}"
+	force_destroy = true
+}
+# Set private bucket and versioning enabled
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.b.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
---
+++ s3_bucket/variable.tf
@@ -0,0 +1,14 @@
+variable "s3_bucket_name" {
+	description = "Name of S3 bucket"
+	type = string
+}
+
+variable "acl_value" {
+	description = "value of ACL"
+	type = string
+}
+
+variable "s3_region" {
+	description = "Region of S3 bucket"
+	type = string
+}
